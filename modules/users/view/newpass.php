a5cb673569cbe354895503291eed940b4dec123f
lluismat <mataix.lluis@gmail.com>
2016-12-15 19:34:32 +0100

    activa la cuenta correctamente, el restore password envia el correo para cambiar la password

--- modules/users/view/newpass.php
+++ modules/users/view/newpass.php
@@ -7,16 +7,16 @@
         <br />
         <div class="form-title"><h2 class="form-contact-heading">Cambia tu contraseña</h2></div>
         <p>Por favor introduce tu nueva contraseña.</p>
-
+        <br>
         <div class="control-group">
             <input type="password" id="password" name="password" placeholder="Password" class="input-block-level" maxlength="100">
-        </div>
+        </div><br>
         <div class="control-group">
             <input type="password" id="password2" name="password2" placeholder="Repit your Password" class="input-block-level" maxlength="100">
-        </div>
+        </div><br>
 
         <input class="btn btn-primary" type="button" name="submit" id="changeBtn" value="Enviar"/>
-
+        <br>
         <img src="<?php echo CONTACT_IMG_PATH; ?>ajax-loader.gif" alt="ajax loader icon" class="ajaxLoader" /><br/><br/>
 
         <div id="resultMessage" style="display: none;"></div>
